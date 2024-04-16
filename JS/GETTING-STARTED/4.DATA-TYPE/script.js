afa0829619ece86e99d8fa63d6f20e6f7f3cd057
barash sharma <138241217+barash1311@users.noreply.github.com>
2024-04-16 17:33:22 +0530

    using data types

---
+++ JS/GETTING-STARTED/4.DATA-TYPE/script.js
@@ -0,0 +1,28 @@
+const age = 24;
+console.log(age);
+//using const
+const yourname = "barash";
+console.log(yourname);
+//using boolean
+const isFinite = true;
+// isFinite = false;
+console.log(isFinite);
+//
+let numofchilderen;
+console.log(numofchilderen);
+const salary = null;
+console.log(salary);
+
+//non primitive type
+
+//object arrays array of object
+const user = {
+  firstname: "jana",
+  lastname: "shi",
+  age: 28,
+};
+console.log(user);
+
+//arrays
+const number = [19, "jane", null, true, undefined, {}];
+console.log(number);
