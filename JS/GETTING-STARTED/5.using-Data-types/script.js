0cd551d04200e265608164b02f170d502dae361c
barash sharma <138241217+barash1311@users.noreply.github.com>
2024-04-16 17:58:15 +0530

    arithmatic-oprators

---
+++ JS/GETTING-STARTED/5.using-Data-types/script.js
@@ -0,0 +1,16 @@
+let a = 2;
+let b = 3;
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a++);
+console.log(a);
+console.log(a--);
+console.log(a);
+let score = 5;
+score += 1;
+console.log(score);
+score -= 1;
+console.log(score);
