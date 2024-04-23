d9c0f2f13b7572ec38472e39247cf11d7fcf5f63
barash sharma <138241217+barash1311@users.noreply.github.com>
2024-04-23 21:04:31 +0530

    commited las tchange

--- JS/arrays/array2.js
+++ JS/arrays/array2.js
@@ -4,6 +4,6 @@ let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 //   console.log(array[i]);
 // }
 //using foreach loop
-array.forEach(function (array) {
+array.forEach(function (element, index, array) {
   console.log(array);
 });
