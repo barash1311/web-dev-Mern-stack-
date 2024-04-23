e5a444c7f712daa2c47df95fcc7a5e36444f455e
barash sharma <138241217+barash1311@users.noreply.github.com>
2024-04-23 14:10:09 +0530

    strings of javascript

---
+++ JS/project2-concatination/scripts.js
@@ -0,0 +1,24 @@
+//using + opearit
+// const firstname = "john";
+// const lastname = "doe";
+// const fullname = firstname + " " + lastname;
+// console.log(fullname);
+// const str1 = "hello";
+// const str2 = "world";
+// const combined = str1.concat(" " + str2);
+// console.log(combined);
+// const fruot="frout";
+// const colour="red";
+
+const lowercasestr = "Hello, world";
+const result = lowercasestr.toLocaleUpperCase();
+const result2 = lowercasestr.toLocaleLowerCase();
+console.log(result);
+console.log(result2);
+const str1 = "javascript";
+const str2 = "JAVASCRIPT";
+if (str1.toLocaleLowerCase() == str2.toLocaleUpperCase()) {
+  console.log("The strings are equal");
+} else {
+  console.log("not equal");
+}
