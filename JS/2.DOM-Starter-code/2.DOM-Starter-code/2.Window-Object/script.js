e72d9ba1ab89d59116726a18fa0b404cf7fd0cc4
barash sharma <138241217+barash1311@users.noreply.github.com>
2024-04-24 23:27:00 +0530

    dom update

---
+++ JS/2.DOM-Starter-code/2.DOM-Starter-code/2.Window-Object/script.js
@@ -0,0 +1,11 @@
+console.log(window.document);
+console.log(window.location.host);
+console.log(window.history);
+console.log(window.navigator);
+console.log(window.screen);
+console.log(window.alert("hii"));
+console.log(window.confirm());
+
+window.prompt();
+window.open("www.google.com");
+console.log(window.body);
---
+++ JS/2.DOM-Starter-code/2.DOM-Starter-code/3.getElementById/script.js
@@ -0,0 +1,14 @@
+const conferenceTitleEl = document.getElementById("title");
+const conferenceDateEl = document.getElementById("conferenceDate");
+const locationTitleEl = document.getElementById("locationTitle");
+const conferenceLocationEl = document.getElementById("conferencelocation");
+const speakersTitleEL = document.getElementById("speakersTitle");
+const speakerListEl = document.getElementById("speakerlist");
+console.log(
+  conferenceTitleEl,
+  conferenceDateEl,
+  locationTitleEl,
+  conferenceLocationEl,
+  speakerListEl,
+  speakersTitleEL
+);
