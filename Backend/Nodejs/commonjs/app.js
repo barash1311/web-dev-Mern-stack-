7a6c16e077ccb57426587a2504226f51c8b8066c
barash sharma <138241217+barash1311@users.noreply.github.com>
2024-04-28 12:46:37 +0530

    update to nodejs
    common js

--- Backend/Nodejs/commonjs/app.js
+++ Backend/Nodejs/commonjs/app.js
@@ -1,5 +1,12 @@
-// const firstname = require("./utilities");
+//
+// const { myadd, mysubtract } = require("./utilities");
+console.log("application started");
+// const { add, subtract } = utils;
+// console.log(utils.add(10, 5));
+// console.log(utils.subtract(10, 5));
 
-import temp from "./utilities";
-console.log("app start here");
-console.log(temp);
+// console.log(myadd(2, 3));
+
+const { sayhi, saybye } = require("./utilities");
+console.log(sayhi("barsh"));
+console.log(saybye("barsh"));
--- Backend/Nodejs/commonjs/utilities.js
+++ Backend/Nodejs/commonjs/utilities.js
@@ -1,6 +1,35 @@
-//single function import
-const firstname = "barash";
-export default firstname;
-//!object import
-//--named function import
-// suing the export
+//single function
+//object
+//single function
+// const firstname = "barash";
+// const greet = (name) => {
+//   console.log(`hello ${firstname}`);
+// };
+// module.exports = greet;
+//object export
+// module.exports = {
+//   add: function (a, b) {
+//     return a + b;
+//   },
+//   subtract: function (a, b) {
+//     return a - b;
+//   },
+// };
+// function add(a, b) {
+//   return a + b;
+// }
+// function subtract(a, b) {
+//   return a - b;
+// }
+// module.exports = {
+//   mysubtract: subtract,
+//   myadd: add,
+// };
+//named function exprt
+exports.sayhi = (name) => {
+  return `hi ${name}`;
+};
+exports.saybye = (name) => {
+  return `bye ${name}`;
+};
+//using short hand
